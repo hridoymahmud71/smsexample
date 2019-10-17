2fbd8acc4723191c64aaf6e5d0d18d320a2d8842
hridoymahmud71 <hridoymahmud71@gmail.com>
2019-10-17 18:13:11 +0600

    first

---
+++ app/src/main/java/hridoy/aiz/smsexample/MainActivity.java
@@ -0,0 +1,13 @@
+package hridoy.aiz.smsexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
